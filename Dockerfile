435fb0c2d9b055febb132fa9a08a59c8e5df126d
nickh <nmhendri@gmail.com>
2024-07-01 15:27:23 -0400

    no idea

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ghcr.io/nhendri/devtooling:latest
+
+COPY entrypoint.sh ./entrypoint.sh
+
+RUN chmod +x entrypoint.sh
+
+CMD [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function main () {
+    
+    which ansible > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+    printf "ERROR   ansible not found on path. exiting...\n"
+    exit 1
+    else
+        printf "INFO    ansible validated...\n"
+    fi
+    
+    local PARAM_LIST=(ENV_VAR01 ENV_VAR02)
+    local EMPTY_PARAMS=0
+
+    for PARAM in ${PARAM_LIST[*]}; do
+        local PARAM_VALUE="${!PARAM}"
+        if [[ $PARAM_VALUE == "" ]]; then
+            case $PARAM in
+                ENV_VAR01)
+                    printf "ERROR   Required parameter $PARAM not set...\n"
+                    EMPTY_PARAMS=1
+                ;;
+                *)
+                    printf "WARNING Optional parameter $PARAM not set...\n"
+                ;;
+            esac
+            else
+                printf "INFO    Parameters are set...\n"
+        fi
+    done
+
+    if [ $EMPTY_PARAMS -eq 1 ]; then
+        printf "ERROR   A required parameter was not set...\n"
+        exit 1
+    fi
+
+    printf "var01: $ENV_VAR01 | var02: $ENV_VAR02\n"
+}
+
+main
